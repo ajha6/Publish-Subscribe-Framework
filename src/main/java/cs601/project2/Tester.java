6beb8becf049c70e93b7445ac6b707c30578ad5c
Anurag Jha <ajha6@dons.usfca.edu>
2018-09-20 03:04:04 -0700

    created initial structure

---
+++ src/main/java/cs601/project2/Tester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cs601.project2;
+
+/**
+ * @author anuragjha
+ * Initial class to start 
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("project2");
+
+	}
+
+}
